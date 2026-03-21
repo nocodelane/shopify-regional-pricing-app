53f6e7fad8eda11109def1fecdddb4e4dda0f6df
sachin <sachin@sachinburman.com>
2026-03-21 22:25:24 +0530

    fix: bypass shopify-api ts mismatch

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -14,6 +14,7 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
+  // @ts-ignore: Bypassing nested shopify-api false positive type mismatch
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
   future: {
